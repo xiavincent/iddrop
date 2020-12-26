d56f29fab10ae0efcbdd40f8a5971f8d7eb792b4
vxia <vxia@stanford.edu>
2020-12-25 23:02:02 -0500

    remove automatic cameral removal (not necessary with edge-based segmentation)

--- ImageProcessing/edgeDetection/Helpers/FilmProcessing/processImages.m
+++ ImageProcessing/edgeDetection/Helpers/FilmProcessing/processImages.m
@@ -1,13 +1,13 @@
 %% FUNCTION
 % Batch process a cell array of images and display the segmented film area
-function processImages(start_index,finish_index,images,area_mask,camera_mask)
+function processImages(start_index,finish_index,images,area_mask)
     len = finish_index - start_index + 1; % length of output cell array
     overlay = cell([1 len]); % store the final images
     
 %     ticBytes(gcp) % report byte transfer of process
     for (i=start_index:finish_index) % parallel 'for' loop for increased processing speed
         RGB = images{i}.frame;
-        overlay{i} = getFilmOverlay(RGB,area_mask,camera_mask);
+        overlay{i} = getFilmOverlay(RGB,area_mask);
     end
 %     tocBytes(gcp)
     
@@ -21,9 +21,8 @@ end
 
 %% PRIVATE HELPER FUNCTIONS
 % Get an overlay of the film for an RGB image
-function overlay = getFilmOverlay(RGB_img,area_mask,camera_mask)    
+function overlay = getFilmOverlay(RGB_img,area_mask)    
     gray = rgb2gray(RGB_img);
-    gray = rmShadow(gray,camera_mask); % remove the camera shadow with inward interpolation
     
     film_mask = findFilm(gray,area_mask); % return a mask of the wet film
     film_mask(~area_mask) = 0;
