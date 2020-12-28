0b93035c49f8ac07b716e5dcbfa118e4078271a4
vxia <vxia@stanford.edu>
2020-12-28 16:05:52 -0500

    implement automatic crop rectangle

--- ImageProcessing/HelperFunctions/VideoHandling/startVideo.m
+++ ImageProcessing/HelperFunctions/VideoHandling/startVideo.m
@@ -1,8 +1,10 @@
 % Get video parameters, set cropping rectangle, and save background frame
-function [crop_rect, vid] = startVideo(file_name,background_frame_num)
+function [crop_rect, vid] = startVideo(file_name,background_frame_num,area_frame_num)
     vid = VideoReader(file_name); % starts reading video
     bg_frame = read(vid, background_frame_num); %gets background frame in video (used for deleting background). background frame is when dome crosses interface
-    crop_rect = getCropSize(bg_frame);
+    
+    area_frame = read(vid,area_frame_num);
+    crop_rect = getCropSize(area_frame);
 end
 
 
@@ -11,19 +13,19 @@ end
 % get user-specified cropping rectangle
 function crop_rect = getCropSize(frame)
 
-%     dome_mask = findDome(frame);
-%     
-%     bounding_box = regionprops(dome_mask,'BoundingBox'); % get rectangle coordinates of minimum bounding box
-%     
-%     min_coord = [bounding_box(1) bounding_box(2)] - 15; % leave a 15 pixel padding on all sides
-%     width_height = [bounding_box(3) bounding_box(4)] + 30;
-%     
-%     crop_rect = [min_coord width_height];
+    dome_mask = findDome(frame);
+    
+    stats = regionprops(dome_mask,'BoundingBox'); % get rectangle coordinates of minimum bounding box
+    
+    min_coord = [stats.BoundingBox(1) stats.BoundingBox(2)] - 15; % leave a 15 pixel padding on all sides
+    width_height = [stats.BoundingBox(3) stats.BoundingBox(4)] + 30;
+    
+    crop_rect = [min_coord width_height];
 
-    figure('Name','Cropping rectangle');
-    dim = [.2 0 .1 .1];
-    str = 'Crop the image to the smallest rectangle containing the dome. Double click when done.';
-    annotation('textbox',dim,'String',str,'FitBoxToText','on','BackgroundColor','white');
-    [~, crop_rect] = imcrop(frame);
-    close('Cropping rectangle');
+%     figure('Name','Cropping rectangle');
+%     dim = [.2 0 .1 .1];
+%     str = 'Crop the image to the smallest rectangle containing the dome. Double click when done.';
+%     annotation('textbox',dim,'String',str,'FitBoxToText','on','BackgroundColor','white');
+%     [~, crop_rect] = imcrop(frame);
+%     close('Cropping rectangle');
 end
--- ImageProcessing/HelperFunctions/edgeDetection/Helpers/DomeProcessing/findDome.m
+++ ImageProcessing/HelperFunctions/edgeDetection/Helpers/DomeProcessing/findDome.m
@@ -1,17 +1,17 @@
 % Find the exposed dome from an RGB image and return a mask of it
 function dome_mask = findDome(RGB_img)
-    gray_img = rgb2gray(RGB_img);
-    [Gmag,~] = imgradient(gray_img);
-    
-    mag_img = mat2gray(Gmag); % convert to grayscale image
-    binary = imbinarize(mag_img,'adaptive'); % turn grayscale into binary image
-    binary_clean = bwareaopen(binary,200);
-        
-    dome = imfill(binary_clean,'holes');
-    clean_size = 500; % remove objects smaller than 500 pixels
-    dome_mask = bwareaopen(dome,clean_size);
-    dome_mask = imclearborder(dome_mask); % clear the border of the mask
+%     gray_img = rgb2gray(RGB_img);
+%     [Gmag,~] = imgradient(gray_img);
+%     
+%     mag_img = mat2gray(Gmag); % convert to grayscale image
+%     binary = imbinarize(mag_img,'adaptive'); % turn grayscale into binary image
+%     binary_clean = bwareaopen(binary,200);
 
-    ecc_range = [0 .5];
-    dome_mask = bwpropfilt(dome_mask,'Eccentricity',ecc_range); % filter out objects outside of the range, leaving only the dome
+    grayscale_img = rgb2gray(RGB_img);
+    edges = getEdges(grayscale_img); % get the edges
+    dome = imfill(edges,'holes');
+    
+    area_range = [100000 300000]; % acceptable dome size between 100k and 300k pixels
+    dome_mask = bwareafilt(dome,area_range);
+    
 end
