2b1739db41ade8a2f66fa4da57046ef5b74c94e6
vxia <vxia@stanford.edu>
2021-01-13 08:56:00 -0800

    paste in writeAnimatedPlot helper function from film area tracking code

--- filmTracking/trackRGB_full.m
+++ filmTracking/trackRGB_full.m
@@ -27,9 +27,34 @@ point.data = trackPoint(point.location, vid); % run through the video, read in e
                   % REACH: output a video that highlights the point of interest in the original
                   % video, so we know what we're tracking
                   
+                  
+% TODO: copy the writeAnimatedPlot algorithm
+                  
 makeVideo(fig,point.data,point.location,file_name,vid);
                   
 %% PRVIATE HELPER FUNCTIONS
+
+function writeAnimatedPlot(file_name_short,output_framerate,area_data_output)
+    % write an animated plot of the wet area data
+    
+    fhandle = makeBlankAreaPlot();
+    
+    animated_vid=VideoWriter(strcat(file_name_short,'_plot'),'MPEG-4');
+    animated_vid.FrameRate = output_framerate;
+    open(animated_vid);
+    for entry=1:length(area_data_output) % iterate through each data point
+        time = area_data_output(2,entry);
+        area = area_data_output(3,entry);
+        plot(time,area,'o',... % plot the graph time and the wet area 
+                'MarkerSize',5,...
+                'MarkerEdgeColor','b'); % marker color = blue
+
+        writeVideo(animated_vid,getframe(gcf));
+    end
+    close(animated_vid); % end the video
+    close(fhandle); % close the figure
+
+end
                   
 
 
