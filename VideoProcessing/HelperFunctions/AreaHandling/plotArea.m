f296005f2b83e4080f9bec6cbe6ef53e8509365e
vxia <vxia@stanford.edu>
2021-01-18 12:50:21 -0800

    fix pdf graph saving file format

--- VideoProcessing/HelperFunctions/AreaHandling/plotArea.m
+++ VideoProcessing/HelperFunctions/AreaHandling/plotArea.m
@@ -6,6 +6,6 @@ function plotArea(area_data_output,file_name_short)
     area = area_data_output(3,:);
     plot(time,area,'.',...
         'MarkerSize',5); % plot our data
-    print(fhandle,strcat(file_name_short,'_graph'),'-dpdf'); % save graph to pdf
+    print(fhandle,strcat(file_name_short,'_graph.pdf'),'-dpdf'); % save graph to pdf
 
 end
