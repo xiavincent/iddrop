7c25926714b91e41759eb5e22715f2eccf644317
vxia <vxia@stanford.edu>
2021-01-17 20:12:19 -0800

    add error message when dome outline can't be detected

--- VideoProcessing/HelperFunctions/edgeDetection/Helpers/DomeProcessing/findDome.m
+++ VideoProcessing/HelperFunctions/edgeDetection/Helpers/DomeProcessing/findDome.m
@@ -7,4 +7,8 @@ function dome_mask = findDome(RGB_img)
     
     area_range = [100000 300000]; % acceptable dome size between 100k and 300k pixels
     dome_mask = bwareafilt(dome,area_range);
+    
+    if (nnz(dome_mask) == 0) % if no acceptable dome was found, throw an error
+        error('Error: No dome outline could be detected! Check input video quality.');
+    end
 end
