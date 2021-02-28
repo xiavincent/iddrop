1b7b67260daf13bcef437b6406f52afa85582c85
vxia <vxia@stanford.edu>
2021-02-28 14:50:21 -0800

    change method of scaling user-defined area_mask; change definition of max_area

--- VideoProcessing/HelperFunctions/edgeDetection/Helpers/FilmProcessing/closeEdges.m
+++ VideoProcessing/HelperFunctions/edgeDetection/Helpers/FilmProcessing/closeEdges.m
@@ -1,7 +1,7 @@
 %% FUNCTION
 % use line shape to morphologically close the edges of a binary image
-function closed_edges = closeEdges(film_edges)
-    line_len = 2;
+function closed_edges = closeEdges(film_edges,size)
+    line_len = 2*size; % make a dilation structuring element with twice the size to grow the edges
     se0 = strel('line',line_len,0); % horizontal line structuring element 
     se90 = strel('line',line_len,90); % vertical line structuring element
     closed_edges = imdilate(film_edges,[se0 se90]);
