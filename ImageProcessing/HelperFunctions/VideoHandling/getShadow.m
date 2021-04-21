527bdae1af8e562fd308faa6b32018f3b0e8c8f1
vxia <vxia@stanford.edu>
2021-04-21 16:02:41 -0700

    change shadow saturation detection threshold to 90

--- ImageProcessing/HelperFunctions/VideoHandling/getShadow.m
+++ ImageProcessing/HelperFunctions/VideoHandling/getShadow.m
@@ -1,8 +1,8 @@
-% Return binary mask of camera shadow (defined by grayscale intensity below 100)
+% Return binary mask of camera shadow (defined by grayscale intensity below 90)
 function [shadow,shadow_area] = getShadow(RGB_img) 
     gray_img = rgb2gray(RGB_img);
-    thresh = 100;
-    shadow = gray_img < thresh; % extract intensity values less than 100 to get camera shadow
+    thresh = 90;
+    shadow = gray_img < thresh; % extract intensity values less than 90 to get camera shadow
     
     structuring_elem = strel('disk',10);
     shadow = imdilate(shadow,structuring_elem); % expand the shadow area to encompass the edges of the shadow
