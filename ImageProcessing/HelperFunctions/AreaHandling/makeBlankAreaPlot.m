1f37b0bae0affbbe5f9c4b222366c0df2001c716
Vincent Xia <vincent@DN0a1f41e9.SUNet>
2021-10-10 16:29:03 -0700

    Allow analysis of 30 min of video

--- ImageProcessing/HelperFunctions/AreaHandling/makeBlankAreaPlot.m
+++ ImageProcessing/HelperFunctions/AreaHandling/makeBlankAreaPlot.m
@@ -4,7 +4,7 @@ function figure_handle = makeBlankAreaPlot()
     figure_handle = figure('Name','Area Plot');
     hold on;
     
-    xlim([0 600]);
+    xlim([0 1800]);
     ylim([0 1.2]);
     xlabel('Time (s)','FontSize' , 28,'FontName'   , 'Arial');
     ylabel('Wet Area / Total Area','FontSize' , 20,'FontName'   , 'Arial');
